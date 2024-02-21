013a40fbd165cebc5d1f641d5c2e90ffc6b69759
YuriiSvereda <aderevs@gmail.com>
2024-02-21 20:08:04 +0200

    working version of task 2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -2,9 +2,31 @@
 {
     internal class Program
     {
+        static void Method1()
+        {
+            for(int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Doing first method");
+                Thread.Sleep(300);
+            }
+        }
+        static void Method2()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Doing second method");
+                Thread.Sleep(300);
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            ParallelOptions options = new ParallelOptions();
+            options.MaxDegreeOfParallelism = 2;
+
+            Parallel.Invoke(options, Method1, Method2);
+
+            Console.WriteLine("The main stream is completed");
         }
     }
 }
