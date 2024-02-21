f6bfc05f15c85f3d607c424ec949c0971bcaca97
YuriiSvereda <aderevs@gmail.com>
2024-02-21 20:50:12 +0200

    working version of task 4

---
+++ Task4/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Task4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            int[] bigArr = new int[1_000_000];
+            Random rnd = new Random();
+            Parallel.For(0, bigArr.Length, (int i) => bigArr[i] = rnd.Next());
+            ParallelQuery<int> odd = from element in bigArr.AsParallel()
+                                         where element % 2 != 0
+                                         select element;
+            foreach (var item in odd)
+            {
+                Console.WriteLine(item);
+                Thread.Sleep(10);
+            }
+        }
+    }
+}
